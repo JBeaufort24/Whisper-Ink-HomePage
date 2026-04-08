f0566ec8944486d3e4850c94e5da645f46f2773c
JBeaufort24 <beaufortdigipro26@gmail.com>
2026-04-08 15:09:11 +0200

    Updated the About Section

--- Whisper-Ink.js
+++ Whisper-Ink.js
@@ -26,3 +26,12 @@ const linkAction = () =>{
 }
 
 navLink.forEach(n=> n.addEventListener('click', linkAction))
+
+/*Change Header Style*/
+const scrollHeader = () =>{
+  const header = document.getElementById('header')
+  this.scrollY >= 50 
+  ? header.classList.add('scroll-header')
+  : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
